648e3890839f6d2d43ee5d66976e4dd76be2be23
John Chen <johnchen328@gmail.com>
2020-08-30 13:07:16 -0700

    integrate and blend in logo

--- client/src/components/LandingHeader.js
+++ client/src/components/LandingHeader.js
@@ -5,22 +5,20 @@ import theme from '../styles/theme.js'
 import { ThemeProvider } from '@material-ui/core/styles';
 
 
+
 function LandingHeader(props) {
     return (
         <>
           <ThemeProvider theme={theme}>
             <div className="landing_navbar">
-              {/* <h2>Venmo</h2> */}
+              <img className="landing_logo" src={process.env.PUBLIC_URL + 'zenmo_logo_blue.svg'} alt="" />
 
-              <Button size="small" color="primary">How Venmo Works</Button>
+              <Button size="small" color="primary">How Zenmo Works</Button>
               <Button size="small" color="primary">Business</Button>
               <Button size="small" color="primary">Card</Button>
               <Button size="small" color="primary">Security</Button>
               <Button size="small" color="primary">Contact Us</Button>
               <Button size="small" color="primary">Sign In</Button>
-
-
-
 
             </div>
           </ThemeProvider>
