61af19d29e48217e49115aa8aa22544f3582e6dc
John Chen <johnchen328@gmail.com>
2020-08-31 13:17:38 -0700

    added forms to signin page

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import LandingHeader from './LandingHeader';
 import LandingFooter from './LandingFooter';
 import '../styles/signIn.css';
-// import { Button, Divider } from '@material-ui/core';
+import { TextField, Button } from '@material-ui/core';
 import theme from '../styles/theme.js'
 import { ThemeProvider } from '@material-ui/core/styles';
 
@@ -15,7 +16,30 @@ function SignIn() {
             <div className="signin_title">Sign in to Zenmo</div>
             <div className="signin_container">
               <div className="signin_box"></div>
+              <TextField
+                className="signin_email"
+                size="small"
+                label="Email or Mobile">
+                </TextField>
+              <div className="signin_password_container">
+                <TextField
+                  className="signin_password"
+                  size="small"
+                  label="Password">
+                </TextField>
+              </div>
+              <div className="signin_demo_submit">
+                <div>
+                <Button variant="contained" color="primary">Demo</Button>
+                </div>
+                <div>
+                <Button variant="contained" color="primary">Sign In</Button>
+                </div>
+              </div>
+
             </div>
+            <Link to="/signup" className="signin_signup_link">Sign Up</Link>
+
           </div>
           <LandingFooter />
           </ThemeProvider>
