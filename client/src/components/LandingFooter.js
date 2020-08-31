641c7a92fa708ab2f7963d0b3f198328852b5d58
John Chen <johnchen328@gmail.com>
2020-08-31 10:28:50 -0700

    cleaned up footer, added basic app store images.  No functionality yet

--- client/src/components/LandingFooter.js
+++ client/src/components/LandingFooter.js
@@ -7,11 +7,35 @@ import { ThemeProvider } from '@material-ui/core/styles';
 function LandingFooter() {
     return (
         <>
-        <div className="landing_footer_container">
-          <Divider />
+        <Divider style={{marginTop: '50px'}}/>
+        <div className="landing_footer_outer">
+
           <ThemeProvider theme={theme}>
-            <h1>Footer Details, AppStore, Google Play</h1>
-            <h5>Zenmo is a service of PayPal, Inc., a licensed provider of money transfer services (NMLS ID: 910457). All money transmission is provided by PayPal, Inc. pursuant to PayPal, Inc.’s licenses. © 2020 PayPal, Inc.</h5>
+          <div className="landing_footer_container">
+
+            <div className="footer_container">
+              <div className="footer_text">
+                <p>Learn More</p>
+                <p>How it Works</p>
+                <p>Contact Us</p>
+              </div>
+              <div className="footer_text">
+                <p>Company</p>
+                <p>Our Team</p>
+                <p>Jobs</p>
+              </div>
+              <div className="footer_text">
+                <p>Community</p>
+                <p>Blog</p>
+                <p>Help Center</p>
+              </div>
+              <div className="footer_logos">
+                <img className="footer_storelogo" src={process.env.PUBLIC_URL + 'apple-app-store.png'} alt="" />
+                <img className="footer_storelogo" src={process.env.PUBLIC_URL + 'google-play-badge.png'} alt="" />
+              </div>
+            </div>
+            <h5 className="footer_disclaimer">Zenmo is a service of PayPal, Inc., a licensed provider of money transfer services (NMLS ID: 910457). All money transmission is provided by PayPal, Inc. pursuant to PayPal, Inc.’s licenses. © 2020 PayPal, Inc.</h5>
+          </div>
           </ThemeProvider>
         </div>
         </>
