e4664fea1173b02645ccdcc5d5bf0a349d8f6044
John Chen <johnchen328@gmail.com>
2020-09-03 16:25:56 -0700

    added search bar functionality

--- client/src/components/DashHeader.js
+++ client/src/components/DashHeader.js
@@ -3,6 +3,7 @@ import { Link, Redirect } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { createMuiTheme } from '@material-ui/core/styles';
 import SearchBar from './SearchBar.js'
+// import SearchBarSem from './SearchBarSem.js'
 import '../styles/dashheader.css';
 import { Button } from '@material-ui/core';
 import { useDispatch } from 'react-redux'
@@ -38,18 +39,15 @@ function DashHeader() {
           <div className="dash_container">
             <div className="dash_logo_search">
               <Link to="/"><img className="dash_logo" src="https://zenmo-bucket.s3-us-west-1.amazonaws.com/assets/venmo_logo_white.svg" alt="" /></Link>
-              <div><SearchBar /></div>
+              <SearchBar />
             </div>
 
             <div className="dash_account_buttons">
-              <Button size="small" style={{fontColor:"white"}}>Username</Button>
-              <Button size="small" style={{fontColor:"white"}}>Statement</Button>
-              <Button size="small" style={{fontColor:"white"}}>Settings</Button>
-              <Button size="small" style={{fontColor:"white"}}>Help</Button>
-              <Button size="small"
-               style={{fontColor:"white"}}
-               onClick={handleSubmit}
-               >Log Out</Button>
+              <Button size="small">Username</Button>
+              <Button size="small">Statement</Button>
+              <Button size="small">Settings</Button>
+              <Button size="small">Help</Button>
+              <Button size="small" onClick={handleSubmit}>Log Out</Button>
             </div>
           </div>
 
