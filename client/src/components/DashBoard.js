ff074ddc695614c7177e0af1caf50e09314c439e
John Chen <johnchen328@gmail.com>
2020-09-04 16:56:04 -0700

    added post request to allusers

--- client/src/components/DashBoard.js
+++ client/src/components/DashBoard.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Feed from './Feed';
 import DashHeader from './DashHeader'
-import {Link} from 'react-router-dom'
 import Profile from './Profile'
 import '../styles/dashboard.css';
 import { useSelector } from 'react-redux'
