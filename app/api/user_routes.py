25a70f01844b65dd0bfb6a4dc9afbf5ac92d1bff
Greg Lloyd <gergdll@gmail.com>
2020-09-22 16:55:55 -0700

    heroku fix

--- app/api/user_routes.py
+++ app/api/user_routes.py
@@ -8,10 +8,10 @@ import random
 
 user_routes = Blueprint("user", __name__, "")
 
-@user_routes.route('/')
-def index():
-  response = User.query.all()
-  return { "users": [user.to_dict() for user in response]}
+# @user_routes.route('/')
+# def index():
+#   response = User.query.all()
+#   return { "users": [user.to_dict() for user in response]}
 
 @user_routes.route('/allusers', methods=['POST'])
 def allusers():
