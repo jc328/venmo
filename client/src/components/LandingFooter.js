04c4547312cff07d026263caf555b139e0ea0e87
Greg Lloyd <gergdll@gmail.com>
2020-10-16 19:13:31 -0700

    landing page footer fix and merge with gapi

--- client/src/components/LandingFooter.js
+++ client/src/components/LandingFooter.js
@@ -12,28 +12,29 @@ function LandingFooter() {
           <ThemeProvider theme={theme}>
           <div className="landing_footer_container">
             <div className="footer_container">
-              <h3 className="dev">Developed By:</h3>
-              <div className="footer_links_container">
-                <p className="footer_title">John Chen</p>
-                <a href="https://www.linkedin.com/in/john-chen-92714817/" className="footer_links">LinkedIn</a>
-                <a href="https://github.com/jc328" className="footer_links">GitHub</a>
-              </div>
-              <div className="footer_links_container">
-                <p className="footer_title">Robert Estrada</p>
-                <a href="https://www.linkedin.com/in/robertmestrada/" className="footer_links">LinkedIn</a>
-                <a href="https://github.com/robertestrada" className="footer_links">GitHub</a>
-              </div>
-              <div className="footer_links_container">
-                <p className="footer_title">Greg Lloyd</p>
-                <a href="https://www.linkedin.com/in/greglloyd1/" className="footer_links">LinkedIn</a>
-                <a href="https://github.com/Greg001100" className="footer_links">GitHub</a>
-              </div>
-              <div className="footer_logos">
+            <h3 className="dev">Developed By:</h3>
+              <div className="dev devflex">
+                <div className="footer_links_container">
+                  <p className="footer_title">John Chen</p>
+                  <a href="https://www.linkedin.com/in/john-chen-92714817/"  target="_blank" className="footer_links">LinkedIn</a>
+                  <a href="https://github.com/jc328"  target="_blank" className="footer_links">GitHub</a>
+                </div>
+                <div className="footer_links_container">
+                  <p className="footer_title">Robert Estrada</p>
+                  <a href="https://www.linkedin.com/in/robertmestrada/"  target="_blank" className="footer_links">LinkedIn</a>
+                  <a href="https://github.com/robertestrada"  target="_blank" className="footer_links">GitHub</a>
+                </div>
+                <div className="footer_links_container">
+                  <p className="footer_title">Greg Lloyd</p>
+                  <a href="https://www.linkedin.com/in/greglloyd1/" target="_blank" className="footer_links">LinkedIn</a>
+                  <a href="https://github.com/Greg001100"  target="_blank" className="footer_links">GitHub</a>
+                </div>
 
               </div>
+
             </div>
-            <h5 className="footer_disclaimer" style={{marginBottom: 25}}>Zenmo is a service of PayBuddy, Inc., a licensed provider of money transfer services (NMLS ID: 999999). All money transmission is provided by PayBuddy, Inc. pursuant to PayBuddy, Inc.’s licenses. © 2020 PayBuddy, Inc.</h5>
           </div>
+            <h5 className="footer_disclaimer" style={{marginBottom: 25}}>Zenmo is a service of PayBuddy, Inc., a licensed provider of money transfer services (NMLS ID: 999999). All money transmission is provided by PayBuddy, Inc. pursuant to PayBuddy, Inc.’s licenses. © 2020 PayBuddy, Inc.</h5>
           </ThemeProvider>
         </div>
         </>
